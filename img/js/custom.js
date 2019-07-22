0a491caf6257765f68619db7f8558b9063f9e9ac
abubakar9212 <abubakar.siddique@9212@miranz.net>
2019-07-22 13:51:51 +0500

    first commit

---
+++ img/js/custom.js
@@ -0,0 +1,46 @@
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar-fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+
+$('.testimonials').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive:  [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+
+
+
+});
+
+
+$(document).ready(function(){
+    $('#prices td').wrapInner('<div class="pricing-table" />');
+});
